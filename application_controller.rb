89f74136fb06c017b8f51c1cdbb7217f327f055b
Susan Wie <susanhwie@gmail.com>
2016-07-02 21:33:41 +0000

    fixed bugs

--- application_controller.rb
+++ application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/reciperesults' do
-    food_type = params[:food_type]
-    included_ingredients = params[:included_ingredients]
-    excluded_ingredients = params[:excluded_ingredients]
+    food_type = params[:food_type].downcase
+    included_ingredients = params[:included_ingredients].downcase
+    excluded_ingredients = params[:excluded_ingredients].downcase
     @reciperesults_array = recipe_search(food_type, included_ingredients, excluded_ingredients)
     new_recipe_api_call(@reciperesults_array[0])
     # @recipe_api = recipe_link(new_recipe_api_call)
